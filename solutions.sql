1e0cf7dae9d13247c1bf152bfd7f804092ecc80e
LukePadawalker <padurariu.luca@gmail.com>
2024-03-04 17:47:24 +0100

    group by and select exercises done

---
+++ solutions.sql
@@ -0,0 +1,79 @@
+-- per Lunedi 4 Marzo
+
+
+
+
+-- esercizi con SELECT
+
+
+
+-- 1. Selezionare tutti gli studenti nati nel 1990 (160)
+SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT * FROM `courses` WHERE `cfu` > 10;
+
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * FROM `students` WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30;
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND HOUR(`hour`) >= 14;
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(*) AS `num_dipartimenti` FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * FROM `teachers` WHERE `phone` IS NULL;
+
+
+
+
+-- esercizi con GROUP BY
+
+
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT `enrolment_date` AS `anno_iscrizione`, COUNT(`enrolment_date`) AS `numero_iscritti` FROM `students` GROUP BY `anno_iscrizione`;
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address`, COUNT(*) AS `numero_insegnanti` FROM `teachers` GROUP BY `office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`)) AS `media_voto`, `exam_id` FROM `exam_student` GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(*) AS `numero_corsi`, `department_id` AS `id_dipartimento` FROM `degrees` GROUP BY `department_id`;
+
+
+
+-- per Martedi 5 Marzo
+
+
+
+
+-- esercizi con JOIN
+
+
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
+
+
